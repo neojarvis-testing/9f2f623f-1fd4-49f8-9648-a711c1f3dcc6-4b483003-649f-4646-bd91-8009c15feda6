737853dae8792062adbc18d2c8fa61abcb071352
nimeshchat1968 <nimeshchat1968@gmail.com>
2025-04-24 12:48:24 +0000

    feedback service

--- springapp/src/main/java/com/examly/springapp/repository/FeedbackRepo.java
+++ springapp/src/main/java/com/examly/springapp/repository/FeedbackRepo.java
@@ -1,10 +1,12 @@
 package com.examly.springapp.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.examly.springapp.model.Feedback;
 
 
 public interface FeedbackRepo extends JpaRepository<Feedback,Integer>{
+    @Query("select f from Feedback f where f.user.userId=?1")
+    public Feedback findByUserId(int userId);
 
 }
