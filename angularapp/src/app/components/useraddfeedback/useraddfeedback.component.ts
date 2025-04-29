25f55560bf974aeba6e8dd6b7099ea02bfe3c1d6
nimeshchat1968 <nimeshchat1968@gmail.com>
2025-04-29 12:56:16 +0000

    userfeedback initial

--- angularapp/src/app/components/useraddfeedback/useraddfeedback.component.ts
+++ angularapp/src/app/components/useraddfeedback/useraddfeedback.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Feedback } from 'src/app/models/feedback.model';
+import { FeedbackService } from 'src/app/services/feedback.service';
 
 @Component({
   selector: 'app-useraddfeedback',
@@ -6,8 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./useraddfeedback.component.css']
 })
 export class UseraddfeedbackComponent implements OnInit {
+  feedbacks:Feedback[]=[]
+  foodName:
+  constructor(private service:FeedbackService) { }
 
-  constructor() { }
+
+  onSubmit(form:NgForm){
+    if(form.valid){
+      this.service.sendFeedback(this.feedback).subscribe((data)=>{
+        this.feedbacks.push(data)
+      })
+    }
+    form.reset()
+  }
 
   ngOnInit(): void {
   }
