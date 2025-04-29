19312ef0f2d159f5cbf9b673aa0d266c2a736670
mish-17-7-1 <amish3017@gmail.com>
2025-04-29 17:21:08 +0000

    order

--- angularapp/src/app/components/adminorderschart/adminorderschart.component.ts
+++ angularapp/src/app/components/adminorderschart/adminorderschart.component.ts
@@ -1,33 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { ChartOptions, ChartType, ChartDataset } from 'chart.js';
-//import { Label } from 'ng2-charts';
+// import { Component, OnInit } from '@angular/core';
+// // import { ChartOptions, ChartType, ChartDataset } from 'chart.js';
+// //import { Label } from 'ng2-charts';
 
 
-@Component({
-  selector: 'app-adminorderschart',
-  templateUrl: './adminorderschart.component.html',
-  styleUrls: ['./adminorderschart.component.css']
-})
-export class AdminorderschartComponent implements OnInit {
+// @Component({
+//   selector: 'app-adminorderschart',
+//   templateUrl: './adminorderschart.component.html',
+//   styleUrls: ['./adminorderschart.component.css']
+// })
+// export class AdminorderschartComponent implements OnInit {
 
-  public barChartOptions: ChartOptions = {
-  responsive: true,
-};
-//public barChartLabels: Label[] = ['2021', '2022', '2023', '2024', '2025'];
-public barChartLabels: string[] = ['2021', '2022', '2023', '2024', '2025'];
-public barChartType: ChartType = 'bar';
-public barChartLegend = true;
-public barChartPlugins = [];
+//   public barChartOptions: ChartOptions = {
+//   responsive: true,
+// };
+// //public barChartLabels: Label[] = ['2021', '2022', '2023', '2024', '2025'];
+// public barChartLabels: string[] = ['2021', '2022', '2023', '2024', '2025'];
+// public barChartType: ChartType = 'bar';
+// public barChartLegend = true;
+// public barChartPlugins = [];
 
-public barChartData: ChartDataset[] = [
-  { data: [65, 59, 80, 81, 56], label: 'Pizza' },
-  { data: [28, 48, 40, 19, 86], label: 'Burger' },
-  { data: [18, 48, 77, 9, 100], label: 'Pasta' }
-];
+// public barChartData: ChartDataset[] = [
+//   { data: [65, 59, 80, 81, 56], label: 'Pizza' },
+//   { data: [28, 48, 40, 19, 86], label: 'Burger' },
+//   { data: [18, 48, 77, 9, 100], label: 'Pasta' }
+// ];
 
-constructor() { }
+// constructor() { }
 
-ngOnInit(): void {
-}
-}
+// ngOnInit(): void {
+// }
+// }
 
