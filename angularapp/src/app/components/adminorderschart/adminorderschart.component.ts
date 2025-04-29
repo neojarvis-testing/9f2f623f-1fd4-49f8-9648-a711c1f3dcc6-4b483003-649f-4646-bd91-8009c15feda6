1b48c968d56cf0f92035ce0ae1d4a715beecca3e
mish-17-7-1 <amish3017@gmail.com>
2025-04-29 14:02:09 +0000

    makeorder

--- angularapp/src/app/components/adminorderschart/adminorderschart.component.ts
+++ angularapp/src/app/components/adminorderschart/adminorderschart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartType, ChartDataset } from 'chart.js';
+//import { Label } from 'ng2-charts';
+
 
 @Component({
   selector: 'app-adminorderschart',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminorderschartComponent implements OnInit {
 
-  constructor() { }
+  public barChartOptions: ChartOptions = {
+  responsive: true,
+};
+//public barChartLabels: Label[] = ['2021', '2022', '2023', '2024', '2025'];
+public barChartLabels: string[] = ['2021', '2022', '2023', '2024', '2025'];
+public barChartType: ChartType = 'bar';
+public barChartLegend = true;
+public barChartPlugins = [];
 
-  ngOnInit(): void {
-  }
+public barChartData: ChartDataset[] = [
+  { data: [65, 59, 80, 81, 56], label: 'Pizza' },
+  { data: [28, 48, 40, 19, 86], label: 'Burger' },
+  { data: [18, 48, 77, 9, 100], label: 'Pasta' }
+];
 
+constructor() { }
+
+ngOnInit(): void {
 }
+}
+
