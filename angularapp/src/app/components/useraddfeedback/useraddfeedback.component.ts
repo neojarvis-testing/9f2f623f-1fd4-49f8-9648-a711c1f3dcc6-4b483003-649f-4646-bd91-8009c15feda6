f5d491d7ac513a65802243ce3fe746c860ff34c5
vipul0320 <vipulkanjolia4832@gmail.com>
2025-04-30 04:44:07 +0000

    feed

--- angularapp/src/app/components/useraddfeedback/useraddfeedback.component.ts
+++ angularapp/src/app/components/useraddfeedback/useraddfeedback.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Food } from 'src/app/models/food.model'; // Assume a food model exists
+import { FoodService } from 'src/app/services/food.service'; // Service to fetch food list
 
 @Component({
   selector: 'app-useraddfeedback',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./useraddfeedback.component.css']
 })
 export class UseraddfeedbackComponent implements OnInit {
+  feedbackForm!: FormGroup;
+  foodList: Food[] = [];
+  successMessage: string = '';
+  errorMessage: string = '';
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private foodService: FoodService) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+    this.loadFoodList();
   }
 
+  initializeForm(): void {
+    this.feedbackForm = this.fb.group({
+      food: ['', Validators.required],
+      feedback: ['', [Validators.required, Validators.minLength(10)]],
+      rating: [null, [Validators.required, Validators.min(1), Validators.max(5)]]
+    });
+  }
+
+  loadFoodList(): void {
+    this.foodService.getAllFoods().subscribe({
+      next: (data) => this.foodList = data,
+      error: () => this.errorMessage = 'Failed to load food list'
+    });
+  }
+
+  onSubmit(): void {
+    if (this.feedbackForm.valid) {
+      const feedbackData = this.feedbackForm.value;
+      console.log('Feedback Submitted:', feedbackData);
+      this.successMessage = 'Thank you for your feedback!';
+      this.feedbackForm.reset();
+    } else {
+      this.errorMessage = 'Please fill out all required fields correctly!';
+    }
+  }
 }
--- angularapp/src/app/components/userviewfeedback/userviewfeedback.component.ts
+++ angularapp/src/app/components/userviewfeedback/userviewfeedback.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Feedback } from 'src/app/models/feedback.model'; // Assume a Feedback model exists
+import { FeedbackService } from 'src/app/services/feedback.service'; // Service to fetch feedback
 
 @Component({
   selector: 'app-userviewfeedback',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./userviewfeedback.component.css']
 })
 export class UserviewfeedbackComponent implements OnInit {
+  feedbackList: Feedback[] = [];
+  errorMessage: string = '';
 
-  constructor() { }
+  constructor(private feedbackService: FeedbackService) { }
 
   ngOnInit(): void {
+    this.loadFeedback();
   }
 
+  loadFeedback(): void {
+    this.feedbackService.getALllFeedbacksByUserId().subscribe({
+      next: (data) => {
+        this.feedbackList = data;
+      },
+      error: () => {
+        this.errorMessage = 'Failed to load feedback';
+      }
+    });
+  }
 }
