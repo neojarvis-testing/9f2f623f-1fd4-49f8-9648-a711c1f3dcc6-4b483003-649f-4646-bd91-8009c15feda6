e637d7d41e142f75216477300f5a0f035e56b42a
mish-17-7-1 <amish3017@gmail.com>
2025-05-02 04:51:17 +0000

    confirm pass

--- angularapp/src/app/components/registration/registration.component.ts
+++ angularapp/src/app/components/registration/registration.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/user.model';
@@ -9,32 +9,43 @@ import { AuthService } from 'src/app/services/auth.service';
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent{
+export class RegistrationComponent {
 
   user: User = {
     email: '',
     password: '',
+    confirmPassword: '',
     username: '',
     mobileNumber: '',
     userRole: 'USER'
   };
-  showDialog=false
-  constructor(private readonly authService: AuthService, private readonly router: Router) {}
+  showDialog = false;
+  passwordMismatch = false;
+  mismatchDialog = false;
+
+  constructor(private authService: AuthService, private readonly router: Router) {}
 
   onSubmit(form: NgForm): void {
-    if (form.valid) {
+    this.passwordMismatch = this.user.password !== this.user.confirmPassword;
+    if (this.passwordMismatch) {
+      this.mismatchDialog = true;
+      return;
+    }
+    if (form.valid && !this.passwordMismatch) {
       this.authService.register(this.user).subscribe(() => {
         this.showDialog = true;
       }, (error) => {
-        alert('Registration failed. Try again.');
-        console.error(error);
+        alert('Registration failed.');
       });
     }
   }
-    
+
   onDialogConfirm(): void {
     this.showDialog = false;
-    this.router.navigate(['/login'])
+    this.router.navigate(['/login']);
+  }
+
+  onMismatchDialogConfirm(): void {
+    this.mismatchDialog = false; 
   }
 }
-
